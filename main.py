e8005c89c226c98da3bcd6ea17fb2787c18d4905
Abhinav Mulagada <abhinav@lx7ny7jd2k.lan>
2022-05-25 14:10:14 -0700

    adding entry point for cloud function

---
+++ main.py
@@ -0,0 +1,31 @@
+
+def get_transactions():
+    print("pulling latest transaction data from rpl_transactions")
+'''
+pull in rpl_transaction data, and place into pandas dataframe
+'''
+
+'''
+Identify the most frequently called function signatures. Tag them as “suspicious”.
+most frequently = top 10?
+'''
+
+
+'''
+Tag smart contracts that these functions belong to as “suspicious”.
+'''
+
+
+'''
+Tag all the addresses that call these functions a lot of times as “suspicious”.
+'''
+
+
+'''
+Tag creators of “suspicious” smart contracts as “suspicious”.
+'''
+
+
+'''
+Tag all other smart contracts created by creators of “suspicious” smart contracts as “suspicious”.
+'''
